df34380070934f945780db8c955a9526f0776b14
AyushhKhare <148583408+AyushhKhare@users.noreply.github.com>
2024-11-20 13:36:00 +0530

    Add files via upload

---
+++ src/nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './index.css'
+const Nav = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>TheCouponSell.com</h1>
+      </div>
+      <div className="mid-btn">
+        <ul>
+          <li>Cloths</li>
+          <li>Electronics</li>
+          <li>Beauty</li>
+        </ul>
+      </div>
+      <div className="right-btn">
+        
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
